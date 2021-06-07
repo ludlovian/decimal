b1d1a1f98bac8b5530baf0eeb5fed8c6e7748c8d
Alan Layng <alan@readersludlow.com>
2021-06-07 11:53:21 +0100

    base tests on canonical string representation

--- test/index.mjs
+++ test/index.mjs
@@ -5,21 +5,21 @@ import { inspect } from 'util'
 
 import decimal from '../src/index.mjs'
 
-function equal (dec, [d, p], desc) {
-  assert.is(dec.tuple[0] === d && dec.tuple[1] === p, true, desc)
-}
-
 test('construction', () => {
-  equal(decimal(12.34), [1234n, 2], 'implied precision from number')
+  assert.is(decimal(12.34).toString(), '12.34', 'implied precision from number')
 
-  equal(decimal('12.340'), [12340n, 3], 'explicit precision from string')
+  assert.is(
+    decimal('12.340').toString(),
+    '12.340',
+    'explicit precision from string'
+  )
 
-  equal(decimal('12.34'), [1234n, 2], 'min precision from string')
+  assert.is(decimal('12.34').toString(), '12.34', 'min precision from string')
 
   const x = decimal(12.34)
   assert.is(decimal(x), x, 'pre-converted passed thru')
 
-  equal(decimal([12340n, 3]), [12340n, 3])
+  assert.is(decimal([12340n, 3]).toString(), '12.340', 'constructed from tuple')
 })
 
 test('isDecimal', () => {
@@ -53,61 +53,137 @@ test('representation', () => {
 })
 
 test('change precision', () => {
-  equal(decimal(12.34).precision(3), [12340n, 3], 'increase precision')
+  assert.is(
+    decimal('12.345')
+      .precision(3)
+      .toString(),
+    '12.345',
+    'no change in precision'
+  )
 
-  equal(
-    decimal(12.345).precision(2),
-    [1235n, 2],
+  assert.is(
+    decimal(12.34)
+      .precision(3)
+      .toString(),
+    '12.340',
+    'increase precision'
+  )
+
+  assert.is(
+    decimal(12.345)
+      .precision(2)
+      .toString(),
+    '12.35',
     'decrease precision and round up'
   )
 
-  equal(
-    decimal(12.345).precision(1),
-    [123n, 1],
+  assert.is(
+    decimal(12.345)
+      .precision(1)
+      .toString(),
+    '12.3',
     'decrease precision and no rounding'
   )
 
-  equal(
+  assert.is(
     decimal(12.345)
       .precision(2)
-      .precision(1),
-    [124n, 1],
+      .precision(1)
+      .toString(),
+    '12.4',
     'decrease and round up twice'
   )
 
-  equal(
-    decimal('-1.2345').precision(3),
-    [-1235n, 3],
+  assert.is(
+    decimal('-1.2345')
+      .precision(3)
+      .toString(),
+    '-1.235',
     'negative round away from zero'
   )
 
-  equal(decimal('-1.2345').precision(2), [-123n, 2], 'negative no rounding')
+  assert.is(
+    decimal('-1.2345')
+      .precision(2)
+      .toString(),
+    '-1.23',
+    'negative no rounding'
+  )
 })
 
 test('add', () => {
-  equal(decimal(12.34).add('34.567'), [46907n, 3], 'add with larger precision')
+  assert.is(
+    decimal(12.34)
+      .add('34.567')
+      .toString(),
+    '46.907',
+    'add with larger precision'
+  )
 
-  equal(decimal(12.345).add('34.5'), [46845n, 3], 'add with smaller precision')
+  assert.is(
+    decimal(12.345)
+      .add('34.5')
+      .toString(),
+    '46.845',
+    'add with smaller precision'
+  )
 })
 
 test('sub', () => {
-  equal(decimal(67.89).sub('12.345'), [55545n, 3], 'sub with larger precision')
+  assert.is(
+    decimal(67.89)
+      .sub('12.345')
+      .toString(),
+    '55.545',
+    'sub with larger precision'
+  )
 
-  equal(decimal(56.789).sub('23.4'), [33389n, 3], 'sub with smaller precision')
+  assert.is(
+    decimal(56.789)
+      .sub('23.4')
+      .toString(),
+    '33.389',
+    'sub with smaller precision'
+  )
 })
 
 test('mul', () => {
-  equal(decimal('12.34').mul('3.7'), [4566n, 2])
+  assert.is(
+    decimal('12.34')
+      .mul('3.7')
+      .toString(),
+    '45.66'
+  )
 })
 
 test('div', () => {
-  equal(decimal(87.65).div(2.7), [3246n, 2])
+  assert.is(
+    decimal(87.65)
+      .div(2.7)
+      .toString(),
+    '32.46'
+  )
 
-  equal(decimal('-87.65').div(2.7), [-3246n, 2])
+  assert.is(
+    decimal('-87.65')
+      .div(2.7)
+      .toString(),
+    '-32.46'
+  )
 
-  equal(decimal('87.65').div('-2.7'), [-3246n, 2])
+  assert.is(
+    decimal('87.65')
+      .div('-2.7')
+      .toString(),
+    '-32.46'
+  )
 
-  equal(decimal('-87.65').div('-2.7'), [3246n, 2])
+  assert.is(
+    decimal('-87.65')
+      .div('-2.7')
+      .toString(),
+    '32.46'
+  )
 
   assert.throws(() => decimal(2).div('0'))
 })
